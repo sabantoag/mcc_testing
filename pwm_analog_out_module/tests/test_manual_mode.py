0f63bb0e3d72bb1de611bbfe96c888d7481ba89b
codykapka <78864023+codykapka@users.noreply.github.com>
2025-07-10 16:29:17 -0500

    Add in manual test case

---
+++ pwm_analog_out_module/tests/test_manual_mode.py
@@ -0,0 +1,22 @@
+from mcculw.device_info import DaqDeviceInfo
+from mcculw import ul
+from utils.polynomial import create_polynomial, CALIBRATED_COEFFICIENTS, plot_polynomial
+from utils.daq_interface import measure_voltage
+from utils.db_logger import log_test_result
+
+
+DIGITAL_OUTPUT_CHANNEL = 0  # Digital output pin used to switch SSRs between auto/manual states.
+
+
+def test_manual_mode(daq_device: DaqDeviceInfo):
+    try:
+        pass
+    except Exception as e:
+        print(f"An error occurred during manual mode test: {e}")
+        log_test_result(
+            test_name="Manual Mode Test",
+            result_bool=False,
+            measurement=None,
+            error_message=str(e)
+        )
+        assert False, f"Manual mode test failed with error: {e}"
