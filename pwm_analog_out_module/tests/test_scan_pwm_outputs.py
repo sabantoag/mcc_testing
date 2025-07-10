b18f541e77389ca08388edb73543ba2e0caca871
codykapka <78864023+codykapka@users.noreply.github.com>
2025-07-10 16:23:34 -0500

    Improvements to the scan pwm output test

--- pwm_analog_out_module/tests/test_scan_pwm_outputs.py
+++ pwm_analog_out_module/tests/test_scan_pwm_outputs.py
@@ -2,13 +2,15 @@ from time import sleep
 
 from mcculw.device_info import DaqDeviceInfo
 from mcculw import ul
-from utils.polynomial import create_polynomial, CALIBRATED_COEFFICIENTS
+from utils.polynomial import create_polynomial, CALIBRATED_COEFFICIENTS, plot_polynomial
+from utils.daq_interface import measure_voltage
 from utils.db_logger import log_test_result
 
 
 
 FREQUENCY_HZ = 1000.0  # Frequency in Hz
-CHANNEL_NUM = 0  # TMR0 pin on CTR04 DAQ device
+CHANNEL_NUM = 0  # TMR pin on USB-1608G
+# TODO: This will probably need to be adjusted via testing.
 TOLERANCE = 0.1  # Tolerance for voltage measurement
 
 
@@ -16,9 +18,14 @@ def test_scan_pwm_outputs(daq_device: DaqDeviceInfo):
     try:
         # Create a polynomial from known, good calibration values.
         calibration_polynomial = create_polynomial(CALIBRATED_COEFFICIENTS)
+        plot_polynomial(
+            calibration_polynomial,
+            x_range=(0.0, 1.0),
+            num_points=1000
+        )  # Plot the polynomial for visual verification.
 
         # Duty cycles to be tested on the PWM outputs. Note excluding 0.0 and 1.0.
-        duty_cycles = [round(i * 0.05, 2) for i in range(1, 21)]
+        duty_cycles = [round(i * 0.05, 2) for i in range(1, 20)]
 
         # Depending on device, value of 1.0 (100%) duty cycle may cause error.
         for duty_cycle in duty_cycles:
@@ -30,12 +37,13 @@ def test_scan_pwm_outputs(daq_device: DaqDeviceInfo):
                 print(f'Outputting {actual_frequency} Hz with a duty cycle of {actual_duty_cycle} to channel {CHANNEL_NUM}')
 
                 # Electronic settling time.
-                sleep(5)
+                sleep(2)
 
                 # Enable with analog input reading capabilities present.
-                # # Grab measured voltage values from analog input and compare to calibrated values.
-                # measured_voltage = ul.a_in_32(daq_device.board_num, CHANNEL_NUM)  # Trigger the input
-                # expected_voltage = calibration_polynomial(actual_duty_cycle)  # Grab theoretical voltage from polynomial.
+                measured_voltage = measure_voltage(daq_device, CHANNEL_NUM)
+                expected_voltage = calibration_polynomial(actual_duty_cycle)  # Grab theoretical voltage from polynomial.
+
+                print(f'Measured voltage: {measured_voltage:.2f} V, Expected voltage: {expected_voltage:.2f} V')
 
                 # # Check if measured voltage falls outside of expected calibrated value.
                 # if abs(measured_voltage - expected_voltage) > TOLERANCE:
--- pwm_analog_out_module/utils/polynomial.py
+++ pwm_analog_out_module/utils/polynomial.py
@@ -1,6 +1,8 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
-CALIBRATED_COEFFICIENTS = [-5.9441039, 1.40338961, 0.28746753, 4.94402715]  # x^n + x^(n-1) + ... + x^0
+
+CALIBRATED_COEFFICIENTS = [-2.3475136, 4.6729021, -7.24486402, 4.98339161]  # x^n + x^(n-1) + ... + x^0
 
 def create_polynomial(coefficients: list[int | float]) -> np.poly1d:
     """Creates a polynomial from the given coefficients.
@@ -13,3 +15,23 @@ def create_polynomial(coefficients: list[int | float]) -> np.poly1d:
     """
     polynomial = np.poly1d(coefficients)
     return polynomial
+
+
+def plot_polynomial(polynomial: np.poly1d, x_range: tuple[float, float], num_points: int = 1000):
+    """Plots the polynomial over a specified range.
+
+    Args:
+        polynomial (np.poly1d): The polynomial to plot.
+        x_range (tuple[float, float]): The range of x values to plot the polynomial over.
+        num_points (int): Number of points to use for plotting.
+    """
+    x_values = np.linspace(x_range[0], x_range[1], num_points)
+    y_values = polynomial(x_values)
+
+    plt.plot(x_values, y_values, label=str(polynomial))
+    plt.title('Polynomial Plot')
+    plt.xlabel('x')
+    plt.ylabel('f(x)')
+    plt.grid()
+    plt.legend()
+    plt.show()
