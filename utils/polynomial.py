e66f91f3b7fcc2833881252d90c2b68dd34ed1cc
codykapka <78864023+codykapka@users.noreply.github.com>
2025-07-22 17:16:48 -0500

    Import List from typing module for type hints vs list while type hinting methods

--- utils/polynomial.py
+++ utils/polynomial.py
@@ -1,14 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from typing import List
 
 
 CALIBRATED_COEFFICIENTS = [-2.3475136, 4.6729021, -7.24486402, 4.98339161]  # x^n + x^(n-1) + ... + x^0
 
-def create_polynomial(coefficients: list[int | float]) -> np.poly1d:
+def create_polynomial(coefficients: List[float]) -> np.poly1d:
     """Creates a polynomial from the given coefficients.
 
     Args:
-        coefficients (list[int  |  float]): List of coefficients for the polynomial, starting from the highest degree.
+        coefficients (List[float]): List of coefficients for the polynomial, starting from the highest degree.
 
     Returns:
         np.poly1d: Polynomial returned as a numpy poly1d object.
